f1378815b0bae97a132b5cc0f734636738abfa40
Rusty <rww6@sfu.ca>
2018-10-22 12:36:10 -0700

    Some minor updates

--- SpaceAttack/Assets/Scripts/P_move.cs
+++ SpaceAttack/Assets/Scripts/P_move.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class P_move : MonoBehaviour {
-    public float speed = 5.0f;
+    public float speed = 3.0f;
+    bool speedboost = false; 
    
 
 	// Use this for initialization
@@ -14,6 +15,22 @@ public class P_move : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        if (speedboost == false)
+        {
+            speed = 3.0f;
+        }else {
+            speed = 5.0f;
+        }
+
+        // Speedboost
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speedboost = true;
+        }else{
+            speedboost = false; 
+        }
+
         // Basic Player Movement
         if (Input.GetKey(KeyCode.W)){
             transform.Translate(Vector2.up * speed * Time.deltaTime);
@@ -21,14 +38,15 @@ public class P_move : MonoBehaviour {
         if (Input.GetKey(KeyCode.S)){
             transform.Translate(Vector2.down * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.A))
-        {
+        if (Input.GetKey(KeyCode.A)){
             transform.Translate(Vector2.left * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.D))
-        {
+        if (Input.GetKey(KeyCode.D)){
             transform.Translate(Vector2.right * speed * Time.deltaTime);
         }
+
+
+       
     }
 
     // Collision
