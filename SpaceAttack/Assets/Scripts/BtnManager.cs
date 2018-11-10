14cfd6abe22a78e70ae1cb8522789b5f79ee4a82
Rusty <rww6@sfu.ca>
2018-11-09 20:29:16 -0800

    Update
    -Added a functioning UI system to navigate through different Scenes.
    -Changed the game resolution to better suit mobile. Currently set to 750x 1330 pixels (Native to iPhone6s where this game will be tested)

---
+++ SpaceAttack/Assets/Scripts/BtnManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnManager : MonoBehaviour {
+
+    /*  BtnManager handles all of the UI Button navigations
+    *   1) New Game
+    *   2) High Scores ->  (NOT YET IMPLEMENTED)
+    *   3) How to play ->  (NOT YET IMPLEMENTED)
+    *   4) Return to Main Menu
+    *   6) Exit App
+    * 
+    *   Future additions? -> Achievments, Level Select
+    */
+
+    //  New Game: Load and Initialize level 1
+    public void NewGameBtn(string newGameLevel){
+        Debug.Log("Loading Level 1.");  
+        SceneManager.LoadScene(newGameLevel);
+
+    }
+
+
+    //  Highscores
+    public void HighscoreBtn(string HighscoreLevel){
+        Debug.Log("Loading Highscore level.");
+        SceneManager.LoadScene(HighscoreLevel);
+    }
+
+    //  Tutorial
+    public void TutorialBtn(string TutorialLevel){
+        Debug.Log("Loading Tutorial level.");
+        SceneManager.LoadScene(TutorialLevel);
+    }
+
+    //  Return to Main menu
+    public void ReturnMainBtn(string MainMenu){
+        Debug.Log("Loading Main Menu level.");
+        SceneManager.LoadScene(MainMenu);
+
+    }
+
+
+    //  Exit application
+    public void ExitGameBtn(){
+        Debug.Log("Exiting application.");
+        Application.Quit();
+    }
+
+}
---
+++ SpaceAttack/Assets/Scripts/potatoBoss.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potatoBoss : MonoBehaviour {
+
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+    }
+    // Collision 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "bullet")
+        {
+            Debug.Log("Enemy Destroyed.");
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+
+    }
+}
